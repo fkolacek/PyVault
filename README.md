bb6ecdf31be8adba1361141c553ed5d559303686
Frantisek Kolacek <fkolacek@redhat.com>
2016-08-18 22:10:03 +0200

    Implemented basic functionality
    Signed-off-by: Frantisek Kolacek <fkolacek@redhat.com>

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 Vault
+
+Usage:
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/production/saber.example.com/root password1
+[*] Vault /home/fkolacek/vault.db does not exist, creating one.
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/production/shiny.example.com/root password2
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/stage/kenny.example.com/root password3
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/stage/kenny.example.com/mysql password4
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/stage/lenny.example.com password5
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/nas.home.example.com/root password6
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db '/services/teampass/fkolacek@redhat.com' password7
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db '/services/lastpass/fkolacek@redhat.com' password8
+0 ✓ 0s fkolacek@carby ~ $ cat ~/vault.db | python -mjson.tool
+{
+    "servers": {
+        "nas.home.example.com": {
+            "root": "password6"
+        },
+        "production": {
+            "saber.example.com": {
+                "root": "password1"
+            },
+            "shiny.example.com": {
+                "root": "password2"
+            }
+        },
+        "stage": {
+            "kenny.example.com": {
+                "mysql": "password4",
+                "root": "password3"
+            },
+            "lenny.example.com": "password5"
+        }
+    },
+    "services": {
+        "lastpass": {
+            "fkolacek@redhat.com": "password8"
+        },
+        "teampass": {
+            "fkolacek@redhat.com": "password7"
+        }
+    }
+}
+0 ✓ 0s fkolacek@carby ~ $ vault.py read ~/vault.db /servers/production/saber.example.com/root
+password1
+0 ✓ 0s fkolacek@carby ~ $ vault.py read ~/vault.db /servers/stage/kenny.example.com
+{u'root': u'password3', u'mysql': u'password4'}
+0 ✓ 0s fkolacek@carby ~ $ vault.py read ~/vault.db /
+{u'services': {u'lastpass': {u'fkolacek@redhat.com': u'password8'}, u'teampass': {u'fkolacek@redhat.com': u'password7'}}, u'servers': {u'production': {u'saber.example.com': {u'root': u'password1'}, u'shiny.example.com': {u'root': u'password2'}}, u'nas.home.example.com': {u'root': u'password6'}, u'stage': {u'lenny.example.com': u'password5', u'kenny.example.com': {u'root': u'password3', u'mysql': u'password4'}}}}
+0 ✓ 0s fkolacek@carby ~ $ vault.py read ~/vault.db /404
+2 ✗ 0s fkolacek@carby ~ $ vault.py read ~/vault.db /servers/
+[!] Invalid key (should not end with /)
+2 ✗ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers password9
+0 ✓ 0s fkolacek@carby ~ $ vault.py write ~/vault.db /servers/nas.home.example.com password9
+[!] Entry /servers already exists, delete it first!
+
--- vault.py
+++ vault.py
@@ -7,23 +7,95 @@
 import sys
 import os
 import argparse
+import json
 from pprint import pprint
+
+def printMsg(errorMessage):
+  print "[*] %s" % errorMessage
+
+def printError(errorMessage, errorCode):
+  print "[!] %s" % errorMessage
+  exit(errorCode)
 
 class cParser(argparse.ArgumentParser):
   def error(self, message):
     self.print_help()
     exit(2)
 
-class Store:
+class Store(object):
+
+  filename = None
+  content = {}
 
   def __init__(self, filename):
-    pass
+    self.filename = filename
 
-  def set(self, key, val):
-    pass
+    if not os.path.exists(self.filename):
+      printMsg("Vault %s does not exist, creating one." % self.filename)
+      self.saveFile()
+    else:
+      self.loadFile()
 
-  def get(self, key):
-    pass
+  def write(self, key, val):
+    self.checkKey(key)
+
+    obj = self.content
+    indexes = key.split('/')[1:]
+    indexes_done = []
+    for index in indexes[:-1]:
+      if not isinstance(obj, dict):
+        raise Exception("Entry /%s already exists, delete it first!" % '/'.join(indexes_done))
+
+      if not index in obj:
+        obj[index] = {}
+
+      obj = obj[index]
+      indexes_done.append(index)
+
+    if not isinstance(obj, dict):
+      raise Exception("Entry /%s already exists, delete it first!" % '/'.join(indexes_done))
+
+    obj[indexes[-1]] = val
+
+    self.saveFile()
+
+  def read(self, key):
+    self.checkKey(key)
+
+    obj = self.content
+
+    if key == '/':
+      print obj
+    else:
+      indexes = key.split('/')[1:]
+
+      for index in indexes[:-1]:
+        if index not in obj:
+          exit(2)
+
+        obj = obj[index]
+
+      if indexes[-1] not in obj:
+        exit(2)
+
+      print obj[indexes[-1]]
+
+  def checkKey(self, key):
+    if not key.startswith('/'):
+      raise Exception('Invalid key (should start with /)')
+
+    if key.endswith('/') and key != '/':
+      raise Exception('Invalid key (should not end with /)')
+
+  def loadFile(self):
+    with open(self.filename, 'r') as f:
+      self.content = json.load(f)
+    f.closed
+
+  def saveFile(self):
+    with open(self.filename, 'w') as f:
+      json.dump(self.content, f)
+    f.closed
 
 class Vault(object):
 
@@ -36,8 +108,9 @@ class Vault(object):
 
 Available commands are:
   init     Initialize specified vault
-  set      Set specific entry
-  get      Get specific entry''')
+  read     Read specific entry
+  write    Write specific entry
+  delete   Delete specific entry''')
 
     parser.add_argument("command", help="Subcommand to run")
 
@@ -54,35 +127,38 @@ Available commands are:
     parser.add_argument("filename", nargs=2, help="Path to specified vault")
     args = parser.parse_args()
 
-    try:
-      self.store = Store(args.filename[1])
-    except:
-      pass
+    self.store = Store(args.filename[1])
 
-  def set(self):
-    parser = cParser(description=self.description, add_help=False, usage="vault.py set <filename> <key> <value>")
+  def write(self):
+    parser = cParser(description=self.description, add_help=False, usage="vault.py write <filename> <key> <value>")
     parser.add_argument("filename", nargs=2, help="Path to specified vault")
     parser.add_argument("key", nargs=1, help="Entry key")
     parser.add_argument("value", nargs=1, help="Entry value")
     args = parser.parse_args()
 
-    try:
-      self.store = Store(args.filename[1])
-      self.store.set(args.key[0], args.value[0])
-    except:
-      pass
+    self.store = Store(args.filename[1])
+    self.store.write(args.key[0], args.value[0])
 
-  def get(self):
-    parser = cParser(description=self.description, add_help=False, usage="vault.py get <filename> <key>")
+  def read(self):
+    parser = cParser(description=self.description, add_help=False, usage="vault.py read <filename> <key>")
     parser.add_argument("filename", nargs=2, help="Path to specified vault")
     parser.add_argument("key", nargs=1, help="Entry key")
     args = parser.parse_args()
 
-    try:
-      self.store = Store(args.filename[1])
-      self.store.get(args.key[0])
-    except:
-      pass
+    self.store = Store(args.filename[1])
+    self.store.read(args.key[0])
+
+  def delete(self):
+    pass
 
 if __name__ == '__main__':
-  Vault()
+
+  try:
+    import gnupg
+
+    Vault()
+  except ImportError:
+    printError("Required module 'gnupg' not found. Please install it by running 'pip install python-gnupg' as root.", 1)
+  except Exception, err:
+    printError('%s' % str(err), 2)
+
